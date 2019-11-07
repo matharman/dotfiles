05b4ba8894da9aea9988f18e06893fe10f65544d
Mathew Harman <mathew.harman@gmail.com>
2019-11-07 00:04:35 -0500

    Add mkdir for i3 so stow doesn't own it

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Make dirs that should not be a stow-owned symlink
 mkdir -p ~/.vim/.swap
+mkdir -p ~/.config/i3
 
 for pkg in */; do
     echo "Installing $pkg..."
