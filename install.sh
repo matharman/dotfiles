e2fbe3e56ace385a039338295710a756f6ba1e72
Mathew Harman <Mathew.Harman@m2mdtech.com>
2019-11-20 10:27:56 -0500

    Commit to neovim

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 #curl -sL install-node.now.sh | sudo sh
 
 # Make dirs that should not be a stow-owned symlink
+mkdir -p ~/.vim
 mkdir -p ~/.config/nvim/.swap
 mkdir -p ~/.config/i3
 
@@ -11,3 +12,7 @@ for pkg in */; do
     echo "Installing $pkg..."
     stow $pkg
 done
+
+# Setup symlinks for vanilla vim
+ln -s ~/.config/nvim/init.vim ~/.vim/vimrc
+ln -s ~/.config/nvim/.swap ~/.vim/.swap
