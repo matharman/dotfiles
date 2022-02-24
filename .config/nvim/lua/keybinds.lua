4df1db13acc3b1a33b62990aec25b61cb973e00f
Mathew Harman <mathew.harman@gmail.com>
2022-02-23 22:04:53 -0500

    Improve workflow with new keybinds & plugins

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,4 +1,14 @@
 vim.g.mapleader = " "
 
+-- Easy FZF file search
+vim.api.nvim_set_keymap("n", "<leader>f", "<Cmd>Files<CR>", { noremap = true, silent = true })
+
+-- Easy FZF buffer switch
+vim.api.nvim_set_keymap("n", "<leader>b", "<Cmd>Buffers<CR>", { noremap = true, silent = true })
+
 -- Easy open vimrc
 vim.api.nvim_set_keymap("n", "<leader><leader>v", "<Cmd>e $MYVIMRC<CR>", { noremap = true, silent = true })
+
+-- Easy open nvim config
+local nvim_config_dir = "$HOME/.config/nvim"
+vim.api.nvim_set_keymap("n", "<leader>lu", "<Cmd>Files " .. nvim_config_dir .. "<CR>", { noremap = true })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,6 +3,9 @@ vim.cmd [[ packadd packer.nvim ]]
 require("packer").startup(function()
     use "wbthomason/packer.nvim"
 
+    -- Autogenerate docstrings :O
+    use "danymat/neogen"
+
     -- Colors
     use "nvim-treesitter/nvim-treesitter"
     use "marko-cerovac/material.nvim"
@@ -12,6 +15,7 @@ require("packer").startup(function()
     use "neovim/nvim-lspconfig"
     use "williamboman/nvim-lsp-installer"
 
+    -- NVIM Config Hacking
     use "folke/lua-dev.nvim"
 
     -- Completion
@@ -25,9 +29,32 @@ require("packer").startup(function()
     use "L3MON4D3/LuaSnip"
     use "saadparwaiz1/cmp_luasnip"
 
+    -- Rust goodies
     use "simrat39/rust-tools.nvim"
+
+    -- Git goodies
     use "tpope/vim-fugitive"
+    use { 
+        "TimUntersberger/neogit",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+        },
+    }
+
+    -- Focus mode (nice for RO stuff)
     use "junegunn/goyo.vim"
+
+    -- Motion comments
+    use {
+        "numToStr/Comment.nvim",
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use { "junegunn/fzf", run = "./install --all" }
+    use "junegunn/fzf.vim"
 end)
 
 vim.cmd([[
@@ -40,3 +67,33 @@ vim.cmd([[
 require("mh-cmp")
 require("mh-lsp")
 require("mh-snippets")
+
+local custom_cxx_template = {
+    template = {
+        annotation_convention = "custom",
+        custom =  {
+            { nil, "/// @file", { no_results = true, type = { "file" } } },
+            { nil, "/// @brief $1", { no_results = true, type = { "func", "file" } } },
+            { nil, "", { no_results = true, type = { "file" } } },
+
+            { nil, "/// @brief $1", { type = { "func" } } },
+            { "tparam", "/// @tparam %s $1" },
+            { "parameters", "/// @param %s $1" },
+            { "return_statement", "/// @return $1" },
+        },
+    }
+}
+
+require('neogen').setup {
+  enabled = true,
+  languages = {
+      c = custom_cxx_template,
+      cpp = custom_cxx_template,
+  }
+}
+
+require("neogit").setup {
+    integrations = {
+        diffview = true
+    },
+}
