b4a5eda550789c9f78afc887f2a1a62f267faf26
Mathew Harman <mathew.harman@gmail.com>
2022-06-03 16:58:30 -0400

    keybinds for workflow

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,14 +1,27 @@
 vim.g.mapleader = " "
 
--- Easy FZF file search
-vim.api.nvim_set_keymap("n", "<leader>f", "<Cmd>Files<CR>", { noremap = true, silent = true })
+-- Easy FZF git file search
+vim.keymap.set("n", "<leader>gf", "<Cmd>GFiles<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>f", "<Cmd>Files<CR>", { noremap = true, silent = true })
 
 -- Easy FZF buffer switch
-vim.api.nvim_set_keymap("n", "<leader>b", "<Cmd>Buffers<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>b", "<Cmd>Buffers<CR>", { noremap = true, silent = true })
+
+local nvim_config_dir = "$HOME/.config/nvim"
 
 -- Easy open vimrc
-vim.api.nvim_set_keymap("n", "<leader><leader>v", "<Cmd>e $MYVIMRC<CR>", { noremap = true, silent = true })
+local vimrc_edit_cmd = "<Cmd>tabnew | e " .. nvim_config_dir .. " | vs $MYVIMRC<CR>"
+vim.keymap.set("n", "<leader><leader>v", vimrc_edit_cmd, { noremap = true, silent = true })
 
--- Easy open nvim config
-local nvim_config_dir = "$HOME/.config/nvim"
-vim.api.nvim_set_keymap("n", "<leader>lu", "<Cmd>Files " .. nvim_config_dir .. "<CR>", { noremap = true })
+-- Fugitive workflow
+local function open_git_if_fugitive()
+    if vim.g.loaded_fugitive then
+        vim.api.nvim_command[[tabnew | Git | only]]
+        local current_win = vim.api.nvim_get_current_win()
+
+        vim.api.nvim_command[[vert Git log]]
+        vim.api.nvim_set_current_win(current_win)
+    end
+end
+
+vim.keymap.set("n", "<leader>G", open_git_if_fugitive, { noremap = true, silent = true })
