661983b230934ae569a92980be43a6cc372ab50a
Mathew Harman <mathew.harman@gmail.com>
2022-11-10 01:49:19 -0500

    Add distrobox awareness to vim-tmux-navigator
    TL;DR -- the "ps" command vim-tmux-navigator uses to query processes
    owned by the current tmux pane is useless if we're in the distrobox
    shell, because the ps command runs outside of the container.
    
    The integration here is tight but in essence we create a std path at
    $XDG_RUNTIME_DIR/dbx-tmux, in which each pane hosting a distrobox shell
    records its Container ID and tty within the container. Recording those
    parameters has to be done on shell entry, so it lives in bashrc.
    
    The in-container tty + the container ID can be used to find processes
    within the proper cgroup and pseudoterminal combination to apply the
    same check as vim-tmux-navigator does on the host.

---
+++ .local/bin/is_not_tmux.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Arguments from tmux #{pane_id} #{pane_tty}.
+pane=$1
+tty=$2
+
+ps -o comm= -t $tty | grep "distrobox-enter" >> /dev/null
+if [ $? == 0 ]; then
+    container_id=$(cat $DBX_TMUX_BRIDGE/$pane/container_id)
+    distrobox_tmux_tty=$(cat $DBX_TMUX_BRIDGE/$pane/tty)
+
+    ## This is the same process list generator used by vim-tmux-navigator, with cgroup added.
+    ## Then we filter by the CONTAINER_ID appearing in the cgroup to get only processes inside the DistroBox.
+    ## Finally, the cut step reshapes the list to suit the grep filter down below (also borrowed from vim-tmux-navigator)
+    get_process_list="ps -t $distrobox_tmux_tty -o state= -o comm= -o cgroup= | grep $container_id | cut -d' ' -f1,2"
+else
+    ## Process list generator used by vim-tmux-navigator
+    get_process_list="ps -t $tty -o state= -o comm= -t"
+fi
+
+## grep filter borrowed from vim-tmux-navigator, + fzf added for Ctrl-J Ctrl-K in tui
+eval "$get_process_list" | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?(g?(view|n?vim?x?)(diff)?|fzf)$' >> /dev/null
+exit $?
