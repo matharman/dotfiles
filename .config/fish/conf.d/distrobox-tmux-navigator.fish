b541a026cf8ef46ee6535503df636bf78c137667
Mathew Harman <mathew.harman@gmail.com>
2023-05-15 09:50:09 -0400

    trying out fish

---
+++ .config/fish/conf.d/distrobox-tmux-navigator.fish
@@ -0,0 +1,32 @@
+if not status is-interactive
+    return
+end
+
+if test -e /.dockerenv
+    set -g container_id $(grep "memory:/" < /proc/self/cgroup | sed 's|.*/||')
+ 	## TODO -- get true container ID, not name
+else if test -e /run/.containerenv
+ 	# we're in podman, use this other trick
+ 	set -g container_id $(grep "^id=" /run/.containerenv | cut -d'=' -f2- | tr -d '"')
+end
+
+set -gx DBX_TMUX_BRIDGE $XDG_RUNTIME_DIR/dbx-tmux
+if test -n "$TMUX"; and test -z "$container_id"
+    tmux setenv DBX_TMUX_BRIDGE "$DBX_TMUX_BRIDGE"
+end
+
+mkdir -p "$DBX_TMUX_BRIDGE"
+
+if test -n "$DISTROBOX_ENTER_PATH"
+    set -g dbx_tmux_bridge_ours "$DBX_TMUX_BRIDGE/$TMUX_PANE"
+
+    set tty $(tty)
+
+    mkdir -p $dbx_tmux_bridge_ours
+    echo $container_id > $dbx_tmux_bridge_ours/container_id
+    echo $(tty) > $dbx_tmux_bridge_ours/tty
+end
+
+function remove_ours_on_exit --on-event fish_exit
+    rm -r $dbx_tmux_bridge_ours
+end
---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    set fish_greeting
+    fish_vi_key_bindings
+
+    alias vi="vim"
+    alias vim="nvim"
+    alias dots='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+    if command -v -q rg
+        set -gx FZF_DEFAULT_COMMAND "rg --files --hidden"
+    end
+end
+
+set -gx CMAKE_EXPORT_COMPILE_COMMANDS 1
+set -gx CMAKE_GENERATOR Ninja
+set -gx EDITOR nvim
+
+fish_add_path $HOME/tools
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path /usr/local/go/bin
+
+if command -v -q starship
+    starship init fish | source
+end
