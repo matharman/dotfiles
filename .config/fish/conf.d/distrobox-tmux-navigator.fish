72eb14bb9cd65ae76c7e24ce8fdadb20bf4c28cb
Mathew Harman <mathew.harman@gmail.com>
2023-07-16 21:49:51 -0400

    fix fish distrobox tmux navigator

--- .config/fish/conf.d/distrobox-tmux-navigator.fish
+++ .config/fish/conf.d/distrobox-tmux-navigator.fish
@@ -1,13 +1,13 @@
 if not status is-interactive
-    return
+    exit
 end
 
 if test -e /.dockerenv
-    set -g container_id $(grep "memory:/" < /proc/self/cgroup | sed 's|.*/||')
- 	## TODO -- get true container ID, not name
+    set -g container_id (grep "memory:/" < /proc/self/cgroup | sed 's|.*/||')
+    ## TODO -- get true container ID, not name
 else if test -e /run/.containerenv
- 	# we're in podman, use this other trick
- 	set -g container_id $(grep "^id=" /run/.containerenv | cut -d'=' -f2- | tr -d '"')
+    # we're in podman, use this other trick
+    set -g container_id (grep "^id=" /run/.containerenv | cut -d'=' -f2- | tr -d '"')
 end
 
 set -gx DBX_TMUX_BRIDGE $XDG_RUNTIME_DIR/dbx-tmux
@@ -20,13 +20,13 @@ mkdir -p "$DBX_TMUX_BRIDGE"
 if test -n "$DISTROBOX_ENTER_PATH"
     set -g dbx_tmux_bridge_ours "$DBX_TMUX_BRIDGE/$TMUX_PANE"
 
-    set tty $(tty)
+    set tty (tty)
 
     mkdir -p $dbx_tmux_bridge_ours
     function remove_ours_on_exit --on-event fish_exit
         rm -r $dbx_tmux_bridge_ours
     end
 
-    echo $container_id > $dbx_tmux_bridge_ours/container_id
-    echo $(tty) > $dbx_tmux_bridge_ours/tty
+    echo $container_id >$dbx_tmux_bridge_ours/container_id
+    echo $tty >$dbx_tmux_bridge_ours/tty
 end
