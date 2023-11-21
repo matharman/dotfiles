3ea51e6f80d3ce44160fcdb443238f2a79c6e5db
Mathew Harman <mathew.harman@gmail.com>
2023-11-20 20:33:21 -0500

    expand distrobox tmux navigator for wezterm

--- .config/fish/conf.d/distrobox-tmux-navigator.fish
+++ .config/fish/conf.d/distrobox-tmux-navigator.fish
@@ -10,15 +10,23 @@ else if test -e /run/.containerenv
     set -g container_id (grep "^id=" /run/.containerenv | cut -d'=' -f2- | tr -d '"')
 end
 
-set -gx DBX_TMUX_BRIDGE $XDG_RUNTIME_DIR/dbx-tmux
-if test -n "$TMUX"; and test -z "$container_id"
-    tmux setenv DBX_TMUX_BRIDGE "$DBX_TMUX_BRIDGE"
-end
+set -e DBX_TMUX_BRIDGE
+set -Ux DBX_TMUX_BRIDGE $XDG_RUNTIME_DIR/dbx-tmux
 
 mkdir -p "$DBX_TMUX_BRIDGE"
 
+# Add a prefix for which kind of pane we're managing.
+# NB: Check tmux first, since it may be nested in Wezterm!
+if test -n "$TMUX_PANE"
+    set -g bridge_pane "tmux-$TMUX_PANE"
+else if test -n "$WEZTERM_PANE"
+    set -g bridge_pane "wez-$WEZTERM_PANE"
+end
+
+# Inside a distrobox container shell, DISTROBOX_ENTER_PATH is set.
+# If this variable exists, then we know we're in a distrobox.
 if test -n "$DISTROBOX_ENTER_PATH"
-    set -g dbx_tmux_bridge_ours "$DBX_TMUX_BRIDGE/$TMUX_PANE"
+    set -g dbx_tmux_bridge_ours "$DBX_TMUX_BRIDGE/$bridge_pane"
 
     set tty (tty)
 
--- .local/bin/is_not_tmux.sh
+++ .local/bin/is_not_tmux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ENABLE_DEBUGGING=0
+PANES_WALKER_BRIDGE="$DBX_TMUX_BRIDGE"
 
 ## Arguments from tmux #{pane_id} #{pane_tty}.
 pane=$1
@@ -11,19 +12,20 @@ debug() {
         echo "$@" >> /tmp/is_not_tmux.log
     fi
 }
-debug "pane $pane tty $tty"
+debug "bridge location '$DBX_TMUX_BRIDGE'"
+debug "pane '$pane' tty '$tty'"
 
 ps -o comm= -t $tty | grep "distrobox-enter" >> /dev/null
 if [ $? == 0 ]; then
-    container_id=$(cat $DBX_TMUX_BRIDGE/$pane/container_id)
-    distrobox_tmux_tty=$(cat $DBX_TMUX_BRIDGE/$pane/tty)
+    container_id=$(cat "$PANES_WALKER_BRIDGE/$pane/container_id")
+    container_tty=$(cat "$PANES_WALKER_BRIDGE/$pane/tty")
 
-    debug "tmux_tty $distrobox_tmux_tty container $container_id"
+    debug "container '$container_id' tty '$container_tty'"
 
     ## This is the same process list generator used by vim-tmux-navigator, with cgroup added.
     ## Then we filter by the CONTAINER_ID appearing in the cgroup to get only processes inside the DistroBox.
     ## Finally, the cut step reshapes the list to suit the grep filter down below (also borrowed from vim-tmux-navigator)
-    get_process_list="ps -t '$distrobox_tmux_tty' -o state= -o comm= -o cgroup= | grep $container_id | cut -d' ' -f1,2"
+    get_process_list="ps -t '$container_tty' -o state= -o comm= -o cgroup= | grep $container_id | cut -d' ' -f1,2"
 else
     ## Process list generator used by vim-tmux-navigator
     get_process_list="ps -t '$tty' -o state= -o comm="
