c9a950e104f05932706614f444e7c36410eac813
Mathew Harman <mathew.harman@gmail.com>
2025-12-26 11:08:54 -0500

    update zephyr worktree hook to handle new root

--- .config/fish/conf.d/zephyr_worktree.fish
+++ .config/fish/conf.d/zephyr_worktree.fish
@@ -3,11 +3,15 @@ function __update_west_manifest_path --on-variable PWD --description "Update wes
         return
     end
 
+    set -l new_worktree_root "$HOME/m2md/zephyr-tracker-root"
     set -l worktree_root "$HOME/work/zephyr-tracker-root"
     set -l legacy_root "$HOME/work/zephyr-tracker"
 
     if string match -q "$worktree_root*" "$PWD"
         set -l worktree $worktree_root/(string split -f2 "$worktree_root" "$PWD" | string split -f2 "/")
+        west config --local manifest.path "$worktree" 2>/dev/null
+    else if string match -q "$new_worktree_root*" "$PWD"
+        set -l worktree $new_worktree_root/(string split -f2 "$new_worktree_root" "$PWD" | string split -f2 "/")
         west config --local manifest.path "$worktree" 2>/dev/null
     else if string match -q "$legacy_root*" "$PWD"
         west config --local manifest.path "$legacy_root" 2>/dev/null
